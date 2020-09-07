6b6dcb9c55635b465e9b7928913356917a83ccf6
tanakaryo <ch5cxcxxh@gmail.com>
2020-09-07 15:55:17 +0900

    edit route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,28 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Route::get(アドレス, 関数など(処理));
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('hello/{msg?}', function ($msg='no message.'){
+    $html = <<<EOF
+    <html>
+    <head>
+    <title>Hello</title>
+    <style>
+    body {font-size:16pt; color:#999;}
+    h1 {font-size:100pt; text-align:right; color:#eee;
+        margin:-40px 0px -50px 0px;}
+    </style>
+    </head>
+    <body>
+        <h1>Hello</h1>
+        <p>{$msg}</p>
+        <p>これは、サンプルで作ったページです。</p>
+    </body>
+    </html>
+    EOF;
+    return $html;
 });
