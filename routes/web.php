3628c4161746b0a5b6ab7b529f83bbea792c80f6
tanakaryo <ch5cxcxxh@gmail.com>
2020-09-11 13:08:27 +0900

    use edit

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::get('hello/show', 'HelloController@show');
 Route::get('person', 'PersonController@index');
 Route::get('person/find', 'PersonController@find');
 Route::post('person/find', 'PersonController@search');
+Route::get('person/add', 'PersonController@add');
+Route::post('person/add', 'PersonController@create');
+Route::get('person/edit', 'PersonController@edit');
+Route::post('person/edit', 'PersonController@update');
